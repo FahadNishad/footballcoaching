b289b8f781a7068c3fed4dfb9215cdab76f023f8
FahadNishad <fahadnishad1122@gmail.com>
2024-03-13 13:59:16 +0500

    add same libraries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Add your custom colors here
+        'main-primary': '#00FFF0',
+        'main-dark': '#49BBBD',
+        'custom-red': '#dc3545',
+      },
+      fontSize: {
+        'mianheading': '45px',
+        'secondheading': '20px',  
+      }
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
+});
