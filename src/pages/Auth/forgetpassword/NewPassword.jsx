0247a5461816f04aacdb6328a48582d05011aa42
ammad-amin-1999313 <madekhan007@gmail.com>
2024-03-21 13:23:52 +0500

    user pages

--- src/pages/Auth/forgetpassword/NewPassword.jsx
+++ src/pages/Auth/forgetpassword/NewPassword.jsx
@@ -4,63 +4,67 @@ import cta from "../../../assets/png/CTA.png";
 import { Link } from "react-router-dom";
 import { IoEyeOffOutline } from "react-icons/io5";
 import { FaLock } from "react-icons/fa";
-
+import { Grid } from "@mui/material";
 
 const NewPassword = () => {
   return (
-    <div className="w-full">
-      <div className="md:w-[85%] w-[90%] mx-auto">
-        <div>
-          <div className="flex items-center gap-2 w-[13%] pt-3 ">
-            <span>
-              <HiArrowLeft />
-            </span>
-            <span>Forgot Password</span>
-          </div>
-        </div>
-        <div className="flex py-[80px]">
-          <div className="">
-            <img src={cta} width={589} height={550} alt="cta coaching" />
-          </div>
-          <div className="px-20 py-32 gap-2 flex flex-col ">
-            <h1 className="text-center text-[32px] font-bold">
-              Create New Password
-            </h1>
+    <Grid container justifyContent="center">
+      <Grid item md={12} sm={12} xs={12}>
+        <div className="w-full">
+          <div className="md:w-[85%] w-[90%] mx-auto">
+            <div>
+              <div className="flex items-center gap-2 w-[40%] pt-3 ">
+                <span>
+                  <HiArrowLeft />
+                </span>
+                <span>Forgot Password</span>
+              </div>
+            </div>
+            <Grid container spacing={6} className="py-[80px]" style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Grid item md={6} sm={12} xs={12} xl={5} >
+            <img src={cta} width="100%" alt="cta coaching" />
+          </Grid>
+              <Grid item md={6} sm={12} xs={12} xl={5} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: '50px' }}>
+                <h1 className="text-center text-[32px] font-bold">
+                  Create New Password
+                </h1>
 
-            <label className="px-[10px]">Password</label>
-            <div className="flex justify-between items-center border w-[100%] mx-auto rounded-3xl py-2 px-4" style={{border: "1px solid #59bec5"}} >
-           <span className="flex gap-2 items-center" >
-           <FaLock/>
-              <input
-                className=" text-black "
-                placeholder="Enter new password"
-                style={{ outline: "none" }}
-              />
-           </span>
-              <IoEyeOffOutline />
-            </div>
-            <label className="px-[10px] pt-2">Conform Password</label>
-            <div className="flex justify-between items-center border w-[100%] mx-auto rounded-3xl py-2 px-4" style={{border: "1px solid #59bec5"}} >
-           <span className="flex gap-2 items-center" >
-           <FaLock/>
-              <input
-                className=" text-black "
-                placeholder="Conform new password"
-                style={{ outline: "none" }}
-              />
-           </span>
-              <IoEyeOffOutline />
-            </div>
-            <Link
-              to="/registration"
-              className="border w-full mt-8 mx-auto  rounded-3xl py-2 px-[170px] text-white bg-[#59bec5] "
-            >
-              Login
-            </Link>
+                <label className="px-[10px] w-[73%] ">Password</label>
+                <div className="flex justify-between items-center border w-[70%] mx-auto rounded-3xl py-2 px-4" style={{border: "1px solid #59bec5"}} >
+                  <span className="flex gap-2 items-center" >
+                    <FaLock/>
+                    <input
+                      className=" text-black "
+                      placeholder="Enter new password"
+                      style={{ outline: "none" }}
+                    />
+                  </span>
+                  <IoEyeOffOutline />
+                </div>
+                <label className="px-[10px] pt-2 w-[73%] ">Conform Password</label>
+                <div className="flex justify-between items-center border w-[70%] mx-auto rounded-3xl py-2 px-4" style={{border: "1px solid #59bec5"}} >
+                  <span className="flex gap-2 items-center" >
+                    <FaLock/>
+                    <input
+                      className=" text-black "
+                      placeholder="Conform new password"
+                      style={{ outline: "none" }}
+                    />
+                  </span>
+                  <IoEyeOffOutline />
+                </div>
+                <Link
+                  to="/registration"
+                  className="border w-[70%] mt-8 mx-auto  rounded-3xl py-2 px-[170px] text-white bg-[#59bec5] "
+                >
+                  Login
+                </Link>
+              </Grid>
+            </Grid>
           </div>
         </div>
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   );
 };
 
--- src/pages/Auth/forgetpassword/Select.jsx
+++ src/pages/Auth/forgetpassword/Select.jsx
@@ -2,54 +2,59 @@ import React from "react";
 import { HiArrowLeft } from "react-icons/hi2";
 import cta from "../../../assets/png/CTA.png";
 import chat from "../../../assets/png/chat.png";
-import emailpart from '../../../assets/png/arrow down.png'
+import emailpart from '../../../assets/png/arrow down.png';
 import { Link } from "react-router-dom";
+import { Grid } from '@mui/material';
 
 const Select = () => {
   return (
-    <div className="w-full">
-      <div className="md:w-[85%] w-[90%] mx-auto">
+    <Grid container justifyContent="center">
+      <Grid item md={10} sm={12} xs={12}>
         <div>
           <div className="flex items-center gap-2 w-[14%] pt-3 ">
             <span>
               <HiArrowLeft />
             </span>
-            <span>Fotgot Password</span>
+            <span>Forgot Password</span>
           </div>
         </div>
-        <div className="flex py-[80px]">
-          <div className="">
-            <img src={cta} width={589} height={550} alt="cta coaching" />
-          </div>
-          <div className="px-20  py-28  flex flex-col ">
-            <h1 className="text-[18px] w-[78%] mx-auto ">
-              Select which contact details should we use to reset your password
-            </h1>
-            <div className="flex shadow-custom border-2 border-transparent hover:border-[#2196F3] gap-4 px-2 py-3 rounded-2xl my-3 w-[78%] mx-auto items-center ">
-              <img src={chat} alt="chat-icon" />
-              <div className="flex flex-col gap-1">
-                <h3 className="text-[#757575] text-[14px] font-medium">
-                  via SMS
-                </h3>
-                <h3 className="text-[16px] font-bold">+111111111</h3>
+        <Grid container spacing={6} className="py-[80px]" style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Grid item md={6} sm={12} xs={12} xl={5} >
+            <img src={cta} width="100%" alt="cta coaching" />
+          </Grid>
+          <Grid item md={6} sm={12} xs={12} xl={5} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: '50px' }}>
+            <div className="px-4 py-4">
+              <h1 className="text-[18px]">
+                Select which contact details should we use to reset your password
+              </h1>
+              <div className="flex shadow-custom border-2 border-transparent hover:border-[#2196F3] gap-4 px-2 py-3 rounded-2xl my-3 items-center ">
+                <img src={chat} alt="chat-icon" />
+                <div className="flex flex-col gap-1">
+                  <h3 className="text-[#757575] text-[14px] font-medium">
+                    via SMS
+                  </h3>
+                  <h3 className="text-[16px] font-bold">+111111111</h3>
+                </div>
+              </div>
+              <div className="flex shadow-custom border-2 border-transparent hover:border-[#2196F3] gap-4 px-2 py-3 rounded-2xl my-3 items-center ">
+                <img src={emailpart} alt="chat-icon" />
+                <div className="flex flex-col gap-1">
+                  <h3 className="text-[#757575] text-[14px] font-medium">
+                    via Email
+                  </h3>
+                  <h3 className="text-[16px] font-bold">and***ley@yourdomain.com</h3>
+                </div>
+              </div>
+              <div className="flex justify-center">
+                <Link to='/verify' className="border rounded-3xl py-2 px-[85px] text-white bg-[#59bec5] text-center">
+                  Verify
+                </Link>
               </div>
             </div>
-            <div className="flex shadow-custom border-2 border-transparent hover:border-[#2196F3] gap-4 px-2 py-3 rounded-2xl my-3 w-[78%] mx-auto items-center ">
-              <img src={emailpart} alt="chat-icon" />
-              <div className="flex flex-col gap-1">
-                <h3 className="text-[#757575] text-[14px] font-medium">
-                  via Email
-                </h3>
-                <h3 className="text-[16px] font-bold">and***ley@yourdomain.com</h3>
-              </div>
-            </div>
-            <Link to='/verify' className="border w-[78%] mx-auto rounded-3xl py-2 px-[170px] text-white bg-[#59bec5] ">
-                Verify
-              </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
